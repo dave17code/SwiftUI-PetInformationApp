98d13619a4ab8125607af3f3186b0affbc6bacf0
dave17code <dave17code@gmail.com>
2024-09-03 18:53:37 +0900

    Initial Commit

---
+++ SwiftUI-PetInformationApp/SwiftUI_PetInformationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PetInformationAppApp.swift
+//  SwiftUI-PetInformationApp
+//
+//  Created by 김두운 on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PetInformationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
